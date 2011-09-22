ac7446584fa9fc6d763415c9a32a7d1e4d68ab88
Kasper Meerts <kaspermeerts@gmail.com>
2011-09-22 22:35:57 +0200

    Update orrery to new renderer

--- src/orrery.c
+++ src/orrery.c
@@ -27,13 +27,10 @@ ALLEGRO_DISPLAY *dpy;
 ALLEGRO_EVENT_QUEUE *ev_queue = NULL;
 Shader *shader;
 
-Matrix *projectionMatrix = NULL;
-Matrix *modelViewMatrix = NULL;
-
-GLfloat light_dir[3] = {4, 0, 5};
-GLfloat material_ambient[3] = {0.25, 0.20725, 0.20725};
-GLfloat material_diffuse[3] = {1, 0.829, 0.829};
-GLfloat material_specular[3] = {0.296648, 0.296648, 0.296648};
+Vec3 light_pos = {0, 0, 0};
+GLfloat light_ambient[3] = {0.25, 0.20725, 0.20725};
+GLfloat light_diffuse[3] = {1, 0.829, 0.829};
+GLfloat light_specular[3] = {0.296648, 0.296648, 0.296648};
 GLfloat shininess = 0.088*128;
 
 SolarSystem *solsys;
@@ -86,52 +83,10 @@ static void calcfps()
 
 }
 
-static void upload_uniforms(void)
-{
-
-	glmUniformMatrix(glGetUniformLocation(shader->program, "projection_matrix"), projectionMatrix);
-
-	glmUniformMatrix(glGetUniformLocation(shader->program, "modelview_matrix"), modelViewMatrix);
-
-	/* Lighting */
-	glUniform3fv(glGetUniformLocation(shader->program, "light_dir"), 1, light_dir);
-
-	glUniform3fv(glGetUniformLocation(shader->program, "mat_ambient"), 1, material_ambient);
-	glUniform3fv(glGetUniformLocation(shader->program, "mat_diffuse"), 1, material_diffuse);
-	glUniform3fv(glGetUniformLocation(shader->program, "mat_specular"), 1, material_specular);
-	glUniform1f(glGetUniformLocation(shader->program, "shininess"), shininess);
-
-
-}
-
-static void render(Camera *cam, Mesh *mesh)
-{
-	int i;
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glmLoadIdentity(modelViewMatrix);
-	cam_view_matrix(cam, modelViewMatrix); /* view */
-
-	for (i = 0; i < solsys->num_planets; i++)
-	{
-		Vec3 v = kepler_position_at_time(&solsys->planet[i].orbit, t);
-
-		glmPushMatrix(&modelViewMatrix);
-		glmTranslate(modelViewMatrix, v.x, v.y, v.z);
-		glmScaleUniform(modelViewMatrix, 70000e6);
-		upload_uniforms();
-		glmPopMatrix(&modelViewMatrix);
-
-		glDrawRangeElements(GL_TRIANGLES, 0, mesh->num_vertices - 1, 
-			mesh->num_indices, GL_UNSIGNED_INT, NULL);
-	}
-
-	al_flip_display();
-	calcfps();
-}
-
 int main(int argc, char **argv)
 {
+	int i;
+	Light light;
 	Entity ent;
 	const char *filename;
 	Camera cam;
@@ -152,6 +107,12 @@ int main(int argc, char **argv)
 	mesh = mesh_import(filename);
 	if (mesh == NULL)
 		return 1;
+	for (i = 0; i < mesh->num_vertices; i++)
+	{
+		mesh->vertex[i].x = mesh->vertex[i].x * 70e9;
+		mesh->vertex[i].y = mesh->vertex[i].y * 70e9;
+		mesh->vertex[i].z = mesh->vertex[i].z * 70e9;
+	}
 
 	cam.fov = M_PI/4;
 	cam.left = 0;
@@ -165,25 +126,33 @@ int main(int argc, char **argv)
 
 	glewInit();
 
-	shader = shader_create(STRINGIFY(ROOT_PATH) "/src/teapot.v.glsl", 
-	                       STRINGIFY(ROOT_PATH) "/src/teapot.f.glsl");
+	shader = shader_create(STRINGIFY(ROOT_PATH) "/data/simple.v.glsl", 
+	                       STRINGIFY(ROOT_PATH) "/data/simple.f.glsl");
 	if (shader == NULL)
 		return 1;
 	glUseProgram(shader->program);
 
-	projectionMatrix = glmNewMatrixStack();
-	modelViewMatrix = glmNewMatrixStack();
+	projection_matrix = glmNewMatrixStack();
+	modelview_matrix = glmNewMatrixStack();
+
+	light.position = light_pos;
+	memcpy(light.ambient, light_ambient, sizeof(light_ambient));
+	memcpy(light.diffuse, light_diffuse, sizeof(light_diffuse));
+	memcpy(light.specular, light_specular, sizeof(light_specular));
+	light.shininess = shininess;
 
 	glClearColor(100/255., 149/255., 237/255., 1.0);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
 
-	ent.data.mesh.mesh = mesh;
+	ent.type = ENTITY_MESH;
+	ent.orientation = (Quaternion) {1, 0, 0, 0};
+	ent.mesh = mesh;
 	entity_upload_to_gpu(shader, &ent);
 
 	/* Transformation matrices */
-	cam_projection_matrix(&cam, projectionMatrix);
+	cam_projection_matrix(&cam, projection_matrix);
 
 	/* Start rendering */
 	while(1)
@@ -244,7 +213,26 @@ int main(int argc, char **argv)
 
 		t += 86400;
 
-		render(&cam, mesh);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glmLoadIdentity(modelview_matrix);
+		cam_view_matrix(&cam, modelview_matrix); /* view */
+
+		light_upload_to_gpu(shader, &light);
+
+		glmPushMatrix(&modelview_matrix);
+		for (i = 0; i < solsys->num_planets; i++)
+		{
+			Vec3 v = kepler_position_at_time(&solsys->planet[i].orbit, t);
+			
+			ent.position = v;
+
+			entity_render(shader, &ent);
+		}
+		glmPopMatrix(&modelview_matrix);
+
+		al_flip_display();
+		calcfps();
 	}
 
 out:
@@ -255,8 +243,8 @@ out:
 	free(mesh);
 
 	shader_delete(shader);
-	glmFreeMatrixStack(projectionMatrix);
-	glmFreeMatrixStack(modelViewMatrix);
+	glmFreeMatrixStack(projection_matrix);
+	glmFreeMatrixStack(modelview_matrix);
 	al_destroy_display(dpy);
 	return 0;
 }
