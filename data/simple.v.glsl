669bd109c4202774156784a0d299178647307c55
Kasper Meerts <kaspermeerts@gmail.com>
2011-09-25 14:37:16 +0200

    Small mistake in shader code

--- data/simple.v.glsl
+++ data/simple.v.glsl
@@ -1,11 +1,12 @@
 #version 330 core
 
 uniform mat4 projection_matrix;
-uniform mat4 modelview_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
 
 in vec3 in_position;
 
 void main(void)
 {
-	gl_Position = projection_matrix * modelview_matrix * vec4(in_position, 1);
+	gl_Position = projection_matrix * view_matrix * model_matrix * vec4(in_position, 1);
 }
