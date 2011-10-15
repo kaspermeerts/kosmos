1bc592b14d2fe4e8e54bf3b7d78c83fda173d506
Kasper Meerts <kaspermeerts@gmail.com>
2011-10-16 01:27:11 +0200

    Shader simplification

--- data/lighting.f.glsl
+++ data/lighting.f.glsl
@@ -5,22 +5,23 @@ uniform mat4 uView;
 uniform mat4 uModel;
 uniform vec3 light_pos, light_ambient, light_diffuse, light_specular;
 
-in vec3 L, E, vertNormal;
+in vec3 vNormal;
+in vec3 L, E;
 
 out vec4 oColour;
 
 void main(void)
 {
-	vec3 N, R;
-	float NdotL, RdotE;
-	float shininess = 11.264;
+	vec3 N, R, ambient, diffuse, specular;
+	const float shininess = 11.264;
+	float intensity;
 
-	N = normalize(vertNormal);
+	N = normalize(vNormal);
 	R = reflect(-L, N);
-	NdotL = max(dot(N, L), 0);
-	RdotE = max(dot(R, E), 0);
 
-	oColour = vec4(light_ambient + NdotL * light_diffuse +
-			pow(RdotE, shininess) * light_specular, 1);
+	ambient = light_ambient;
+	diffuse = max(dot(N, L), 0) * light_diffuse;
+	specular = pow(max(dot(R, E), 0), shininess) * light_specular;
+	oColour = vec4(ambient + diffuse + specular, 1);
 
 }
--- data/lighting.v.glsl
+++ data/lighting.v.glsl
@@ -8,20 +8,20 @@ uniform vec3 light_pos, light_ambient, light_diffuse, light_specular;
 in vec3 aPosition;
 in vec3 aNormal;
 
-out vec3 L, E, vertNormal;
+out vec3 vNormal;
+out vec3 L, E;
 
 void main(void)
 {
-	/* Position in eye space */
 	mat4 modelView = uView * uModel;
+	mat3 normal_matrix = inverse(transpose(mat3(modelView)));
 	vec4 eye_pos = modelView * vec4(aPosition, 1.0);
 	vec3 light_dir = light_pos - eye_pos.xyz;
 	gl_Position = uProj * eye_pos;
 
-	mat3 normal_matrix = inverse(transpose(mat3(modelView)));
 	L = normalize(light_dir);
 	E = -normalize(eye_pos.xyz);
-	vertNormal = normal_matrix * aNormal;
+	vNormal = normal_matrix * aNormal;
 }
 
 
